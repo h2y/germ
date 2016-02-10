3b85bab4feee972bf179c094916fbe865949cbaa
hzy <hzy961016@163.com>
2016-02-10 10:30:51 +0800

    添加screenshot.png，版权信息修改，主题设置页面前端jq引用错误修正

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
         </div>
         <footer id="footer" class="yahei clearfix">
-			<p class="right">Theme by <a href="http://lophita.com" target="_blank">Lophita</a></p>
-			<p class="left"><?php echo dopt('d_notice_bottom');?></p>
+  		<p class="left"><?php echo dopt('d_notice_bottom');?></p>
+			<p class="right">Powered by <a href="https://cn.wordpress.org/" target="_blank" rel="nofollow">WordPress</a>, and theme by <a href="http://hzy.pw/p/1933" target="_blank">Moshel</a>.</p>
 		</footer>
 	</div>
-	
+
 	<img id="qrimg" src="http://s.jiathis.com/qrcode.php?url=<?php global $wp;echo home_url(add_query_arg(array(),$wp->request));?>"/>
 	<a id="qr" href="javascript:;"><i class="fa fa-qrcode"></i></a>
 	<a id="gotop" href="javascript:;"><i class="fa fa-arrow-up"></i></a>
-	
-<?php 
+
+<?php
 if( dopt('d_track_b') != '' ) '<div class="static-hide">'.dopt('d_track').'</div>';
 if( dopt('d_footcode_b') != '' ) echo dopt('d_footcode');
 
-if( is_single() && dopt('d_sideroll_single_b') ){ 
+if( is_single() && dopt('d_sideroll_single_b') ){
 	$sr_1 = dopt('d_sideroll_single_1');
 	$sr_2 = dopt('d_sideroll_single_2');
 }elseif( is_home() && dopt('d_sideroll_index_b') ){
@@ -30,6 +30,6 @@ echo '<script>var asr_1 = '.$sr_1.',asr_2 = '.$sr_2.';</script>';
 
 wp_footer();
 
-?>  
+?>
 </body>
 </html>
--- inc/themeset.php
+++ inc/themeset.php
@@ -1,89 +1,89 @@
 <?php
-$themename = 'Quench 主题';
+$themename = 'Germ 主题';
 
 $options = array (
 
 	//基本设置
 	array( "name" => "基本设置","type" => "section","desc" => "主题的基本设置，包括模块是否开启等"),
-	
+
 	array( "name" => "网站描述","type" => "tit"),
-	array( "id" => "d_description","type" => "text","std" => "输入你的网站描述，一般不超过200个字符"),	
-	
+	array( "id" => "d_description","type" => "text","std" => "输入你的网站描述，一般不超过 200 字符"),
+
 	array( "name" => "文章顶部公告","type" => "tit"),
 	array( "id" => "d_notice","type" => "textarea"),
-	
+
 	array( "name" => "页面底部公告","type" => "tit"),
 	array( "id" => "d_notice_bottom","type" => "textarea"),
-	
-	array( "name" => "禁止站内pingback","type" => "tit"),
+
+	array( "name" => "禁止站内 pingback","type" => "tit"),
 	array( "id" => "d_nopingback_b","type" => "checkbox" ),
-	
+
 	array( "name" => "中英文间自动添加空格","type" => "tit"),
 	array( "id" => "d_autospace_b","type" => "checkbox" ),
 
 	array( "name" => "开启全站 Ajax","type" => "tit"),
 	array( "id" => "d_ajax_b","type" => "checkbox" ),
-	
+
 	array( "name" => "使用同一个侧边栏","type" => "tit"),
 	array( "id" => "d_same_sidebar_b","type" => "checkbox" ),
-	
+
 	array( "name" => "首页面滚动时侧栏模块固定","type" => "tit"),
 	array( "id" => "d_sideroll_index_b","type" => "checkbox" ),
 	array( "id" => "d_sideroll_index_1","type" => "number","std" => "1","txt" => "滚动时固定侧栏模块"),
 	array( "id" => "d_sideroll_index_2","type" => "number","std" => "2","txt" => "和模块 "),
-	
+
 	array( "name" => "文章页面滚动时侧栏模块固定","type" => "tit"),
 	array( "id" => "d_sideroll_single_b","type" => "checkbox" ),
 	array( "id" => "d_sideroll_single_1","type" => "number","std" => "1","txt" => "滚动时固定侧栏模块"),
 	array( "id" => "d_sideroll_single_2","type" => "number","std" => "2","txt" => "和模块 "),
-	
+
 	array( "name" => "其他页面滚动时侧栏模块固定","type" => "tit"),
 	array( "id" => "d_sideroll_page_b","type" => "checkbox" ),
 	array( "id" => "d_sideroll_page_1","type" => "number","std" => "1","txt" => "滚动时固定侧栏模块"),
 	array( "id" => "d_sideroll_page_2","type" => "number","std" => "2","txt" => "和模块 "),
-	
+
 	array( "type" => "endtag"),
-	
+
 
 
 	//SNS设置
 	array( "name" => "SNS设置","type" => "section" ),
-	
+
     array( "name" => "开启","type" => "tit"),
 	array( "id" => "d_sns_open","type" => "checkbox" ),
-	
-	array( "name" => "RSS订阅地址","type" => "tit"),
+
+	array( "name" => "RSS 订阅地址","type" => "tit"),
 	array( "id" => "d_rss_b","type" => "checkbox" ),
 	array( "id" => "d_rss","type" => "text","class" => "d_inp_short","std" => "http://mzw.me/feed"),
-	
+
 	array( "name" => "邮箱地址","type" => "tit"),
 	array( "id" => "d_mail_b","type" => "checkbox" ),
 	array( "id" => "d_mail","type" => "text","class" => "d_inp_short","std" => "mailto://i@mzw.me"),
-	
+
     array( "name" => "新浪微博","type" => "tit"),
 	array( "id" => "d_rss_sina_b","type" => "checkbox" ),
 	array( "id" => "d_rss_sina","type" => "text","class" => "d_inp_short","std" => "http://weibo.com/"),
-	
+
 	array( "name" => "twitter","type" => "tit"),
 	array( "id" => "d_rss_twitter_b","type" => "checkbox" ),
 	array( "id" => "d_rss_twitter","type" => "text","class" => "d_inp_short","std" => "http://twitter.com"),
-	
+
 	array( "name" => "Google+","type" => "tit"),
 	array( "id" => "d_rss_google_b","type" => "checkbox" ),
 	array( "id" => "d_rss_google","type" => "text","class" => "d_inp_short","std" => "https://plus.google.com "),
-	
+
 	array( "name" => "Github","type" => "tit"),
 	array( "id" => "d_rss_github_b","type" => "checkbox" ),
 	array( "id" => "d_rss_github","type" => "text","class" => "d_inp_short","std" => "https://github.com/mastermay"),
-	
+
 	array( "name" => "facebook","type" => "tit"),
 	array( "id" => "d_rss_facebook_b","type" => "checkbox" ),
 	array( "id" => "d_rss_facebook","type" => "text","class" => "d_inp_short","std" => "http://facebook.com"),
-	
+
 	array( "name" => "Linkedin","type" => "tit"),
 	array( "id" => "d_rss_linkedin_b","type" => "checkbox" ),
 	array( "id" => "d_rss_linkedin","type" => "text","class" => "d_inp_short","std" => "http://linkedin.com"),
-	
+
 	array( "name" => "腾讯微博","type" => "tit"),
 	array( "id" => "d_rss_tencent_b","type" => "checkbox" ),
 	array( "id" => "d_rss_tencent","type" => "text","class" => "d_inp_short","std" => "http://t.qq.com"),
@@ -93,17 +93,17 @@ $options = array (
 
 
 	//首尾代码
-	array( "name" => "首尾代码","type" => "section" ),	
-	
-	array( "name" => "流量统计代码","type" => "tit"),	
+	array( "name" => "首尾代码","type" => "section" ),
+
+	array( "name" => "流量统计代码","type" => "tit"),
 	array( "id" => "d_track_b","type" => "checkbox" ),
 	array( "id" => "d_track","type" => "textarea","std" => "贴入百度统计、CNZZ、51啦、量子统计代码等等"),
 
-	array( "name" => "头部公共代码","type" => "tit"),	
+	array( "name" => "头部公共代码","type" => "tit"),
 	array( "id" => "d_headcode_b","type" => "checkbox" ),
 	array( "id" => "d_headcode","type" => "textarea","std" => "这部分代码显示在head标签内，可以是css，js等代码"),
 
-	array( "name" => "底部公共代码","type" => "tit"),	
+	array( "name" => "底部公共代码","type" => "tit"),
 	array( "id" => "d_footcode_b","type" => "checkbox" ),
 	array( "id" => "d_footcode","type" => "textarea","std" => "这部分代码显示在页面最底部，可以是js等代码"),
 
@@ -116,12 +116,12 @@ function mytheme_add_admin() {
 	if ( $_GET['page'] == basename(__FILE__) ) {
 		if ( 'save' == $_REQUEST['action'] ) {
 			foreach ($options as $value) {
-				update_option( $value['id'], $_REQUEST[ $value['id'] ] ); 
+				update_option( $value['id'], $_REQUEST[ $value['id'] ] );
 			}
 			/*
 			foreach ($options as $value) {
 				if( isset( $_REQUEST[ $value['id'] ] ) ) { update_option( $value['id'], $_REQUEST[ $value['id'] ]  ); }
-				else { delete_option( $value['id'] ); } 
+				else { delete_option( $value['id'] ); }
 			}
 			*/
 			header("Location: admin.php?page=themeset.php&saved=true");
@@ -143,7 +143,7 @@ function mytheme_admin() {
 	if ( $_REQUEST['reset'] ) echo '<div class="d_message">'.$themename.'已恢复设置</div>';
 ?>
 
- 
+
  <style type="text/css">
 a,input,button,select,textarea{outline:none}
 
@@ -217,19 +217,19 @@ a,input,button,select,textarea{outline:none}
 .d_quicker_menu{color: #666;text-align: center;}
 .d_quicker_menu li{background: none repeat scroll 0 0 #FF6543;
     border: 1px solid #D44B29;
-   
+
     box-shadow: 0 1px 0 rgba(255, 255, 255, 0.25) inset, 0 2px 0 #AF2300, 0 3px 3px rgba(0, 0, 0, 0.5);
     color: #FFFFFF;
     cursor: pointer;
     font-size: 12px;
-    
+
     padding: 4px 12px 3px;
     text-decoration: none;
     text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.3);float:left;margin-right:20px}
 .d_quicker_menu li:hover{background: none repeat scroll 0 0 #FF6543;
     border-color: #C92B00;
     box-shadow: 0 -1px 3px #C92B00 inset, 0 1px 0 rgba(255, 255, 255, 0.25) inset, 0 -1px 0 rgba(0, 0, 0, 0.05) inset, 0 1px 0 rgba(255, 255, 255, 0.75), 0 -1px 0 rgba(0, 0, 0, 0.15);
-    
+
     margin-top: 0;}
 
 .d_port_btn{font-size:12px;font-weight:normal; display:inline-block}
@@ -261,7 +261,7 @@ jQuery(document).ready(function($) {
 			$($('.d_mainbox')[i]).show().siblings('.d_mainbox').hide();
 		})
 	});
-	
+
 	$.fn.extend({
 		insertAtCaret: function(myValue){
 			var $t=$(this)[0];
@@ -271,7 +271,7 @@ jQuery(document).ready(function($) {
 				sel.text = myValue;
 				this.focus();
 			}
-			else 
+			else
 			if ($t.selectionStart || $t.selectionStart == '0') {
 				var startPos = $t.selectionStart;
 				var endPos = $t.selectionEnd;
@@ -287,27 +287,27 @@ jQuery(document).ready(function($) {
 				this.focus();
 			}
 		}
-	}) 
+	})
 
-	
-	
+
+
 })
 </script>
 
 
 
 <div class="wrap d_wrap">
-	
+
 	<h2><?php echo $themename; ?>设置
-		
+
 	</h2>
-	
+
 	<form method="post" class="d_formwrap">
 		<div class="d_tab">
 			<ul>
 				<li class="d_tab_on">基本设置</li>
 				<li>SNS设置</li>
-				<li>首尾代码</li>			
+				<li>首尾代码</li>
 			</ul>
 		</div>
 		<?php foreach ($options as $value) { switch ( $value['type'] ) { case "": ?>
@@ -315,44 +315,44 @@ jQuery(document).ready(function($) {
 			</li><li class="d_li">
 			<h4><?php echo $value['name']; ?>：</h4>
 			<div class="d_tip"><?php echo $value['tip']; ?></div>
-			
+
 			<?php break; case 'text': ?>
 			<input class="d_inp <?php echo $value['class']; ?>" name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" type="<?php echo $value['type']; ?>" value="<?php if ( get_settings( $value['id'] ) != "") { echo stripslashes(get_settings( $value['id'])  ); } else { echo $value['std']; } ?>" />
 
 			<?php break; case 'number': ?>
 			<label class="d_number"><?php echo $value['txt']; ?><input class="d_num <?php echo $value['class']; ?>" name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" type="<?php echo $value['type']; ?>" value="<?php if ( get_settings( $value['id'] ) != "") { echo stripslashes(get_settings( $value['id'])  ); } else { echo $value['std']; } ?>" /></label>
-			
+
 			<?php break; case 'textarea': ?>
 			<textarea class="d_tarea" name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" type="<?php echo $value['type']; ?>" cols="" rows=""><?php if ( get_settings( $value['id'] ) != "") { echo stripslashes(get_settings( $value['id']) ); } else { echo $value['std']; } ?></textarea>
-			
+
 			<?php break; case 'select': ?>
 			<?php if ( $value['desc'] != "") { ?><span class="d_the_desc" id="<?php echo $value['id']; ?>_desc"><?php echo $value['desc']; ?></span><?php } ?><select class="d_sel" name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>">
 				<?php foreach ($value['options'] as $option) { ?>
 				<option <?php if (get_settings( $value['id'] ) == $option) { echo 'selected="selected" class="d_sel_opt"'; } ?>><?php echo $option; ?></option>
 				<?php } ?>
 			</select>
-			
+
 			<?php break; case "checkbox": ?>
 			<?php if(get_settings($value['id']) != ""){ $checked = "checked=\"checked\""; }else{ $checked = "";} ?>
 			<label class="d_check"><input type="checkbox" id="<?php echo $value['id']; ?>" name="<?php echo $value['id']; ?>" <?php echo $checked; ?> />开启</label>
-			
+
 			<?php break; case "section": $i++; ?>
 			<div class="d_mainbox" id="d_mainbox_<?php echo $i; ?>">
 				<ul class="d_inner">
 					<li class="d_li">
-				
+
 			<?php break; case "endtag": ?>
 			</li></ul>
 			<div class="d_desc"><input class="button-primary" name="save<?php echo $i; ?>" type="submit" value="保存设置" /></div>
 			</div>
-			
+
 		<?php break; }} ?>
-				
+
 		<input type="hidden" name="action" value="save" />
 
 	</form>
 
 </div>
-<script src="<?php bloginfo('template_url') ?>/js/jquery-1.7.2.min.js"></script> 
+<script src="<?php bloginfo('template_url') ?>/js/jquery-1.x.min.js"></script>
 <?php } ?>
 <?php add_action('admin_menu', 'mytheme_add_admin');?>

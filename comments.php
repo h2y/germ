54366926e1f404033e6a157a62bd0f7e94231836
Moshel <hzy961016@163.com>
2016-02-13 11:23:43 +0800

    文章评论区域文字汉化

--- comments.php
+++ comments.php
@@ -1,6 +1,6 @@
 <div class="comments clearfix box">
 	<?php $comments = get_comments('post_id='.$post->ID.'&status=approve');?>
-	<h5 id="comments-title"><span><?php echo count($comments); ?> Comments</span></h5>
+	<h5 id="comments-title"><span><?php echo count($comments); ?> 条评论</span></h5>
 	<div class="commentshow">
 		<ol class="comments-list">
 			<?php wp_list_comments('type=comment&callback=comment&max_depth=1000&style=ol'); ?>
@@ -9,29 +9,31 @@
 	</div>
 
 	<div id="respond" class="comment-respond">
-		<h5 id="replytitle" class="comment-reply-title">Leave a Reply <small><a rel="nofollow" id="cancel-comment-reply-link" href="#respond" style="display:none;">Cancel reply</a></small></h5>
+		<h5 id="replytitle" class="comment-reply-title">发表一条评论 <small><a rel="nofollow" id="cancel-comment-reply-link" href="#respond" style="display:none;">取消回复</a></small></h5>
 		<form action="#" method="post" id="commentform" class="clearfix">
 			<?php if ( $user_ID ) { ?>
-			<p style="margin-bottom:10px">Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>&nbsp;|&nbsp;<a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
+			<p style="margin-bottom:10px"><i class="fa fa-user"></i> 已登录为 <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>
+			&nbsp;|&nbsp;
+			<i class="fa fa-mail-reply"></i> <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">登出 &raquo;</a></p>
 			<?php } else { ?>
 			<p class="input-row"><input type="text" name="author" class="text_input" id="author" size="22" tabindex="1" placeholder="<?php _e('NAME', 'quench');?> *"/>
 			</p>
-			
+
 			<p class="input-row"><input type="text" name="email" class="text_input" id="email" size="22" tabindex="2" placeholder="<?php _e('E-MAIL', 'quench');?> *"/>
 			</p>
-			
+
 			<p class="input-row"><input type="text" name="url" class="text_input" id="url" size="22" tabindex="3"  placeholder="<?php _e('WEBSITE', 'quench');?>"/>
 			</p>
-			
+
 			<?php }?>
-			
+
 			<?php comment_id_fields(); do_action('comment_form', $post->ID); ?>
-		
-	
-		
-			<p class="input-row message-row"><textarea class="text_area" rows="3" cols="80" name="comment" id="comment" tabindex="4"  placeholder="<?php _e('CONTENT...', 'quench');?>"></textarea></p>
-			<input type="submit" name="submit" class="button" id="submit" tabindex="5" value="Submit"/>
-		
+
+
+
+			<p class="input-row message-row"><textarea class="text_area" rows="3" cols="80" name="comment" id="comment" tabindex="4"  placeholder="欢迎在这里畅所欲言..."></textarea></p>
+			<input type="submit" name="submit" class="button" id="submit" tabindex="5" value="提交评论"/>
+
 		</form>
 	</div>
 
